255331b352530dfba9658a684296877520c9509c
wen-nguyen <nguyenwt@vcu.edu>
2023-12-15 16:10:24 -0500

    added the remaining sounds

--- final2/script.js
+++ final2/script.js
@@ -21,6 +21,87 @@ $(".chimes-portal").click(function(){
 	$(".four").show();
 });
 
+// VOICE 
+$(".v1").hover
+	(function(){
+		document.getElementById('v1').play();
+});
+
+$(".v2").hover
+	(function(){
+		document.getElementById('v2').play();
+});
+
+$(".v3").hover
+	(function(){
+		document.getElementById('v3').play();
+});
+
+$(".v4").hover
+	(function(){
+		document.getElementById('v4').play();
+});
+
+$(".v5").hover
+	(function(){
+		document.getElementById('v5').play();
+});
+
+$(".v6").hover
+	(function(){
+		document.getElementById('v6').play();
+});
+
+$(".v7").hover
+	(function(){
+		document.getElementById('v7').play();
+});
+
+$(".v8").hover
+	(function(){
+		document.getElementById('v8').play();
+});
+
+$(".v9").hover
+	(function(){
+		document.getElementById('v9').play();
+});
+
+$(".voice-portal").hover
+	(function(){
+		document.getElementById('v10').play();
+});
+
+$(".v11").hover
+	(function(){
+		document.getElementById('v11').play();
+});
+
+$(".v12").hover
+	(function(){
+		document.getElementById('v12').play();
+});
+
+$(".v13").hover
+	(function(){
+		document.getElementById('v13').play();
+});
+
+$(".v14").hover
+	(function(){
+		document.getElementById('v14').play();
+});
+
+$(".v15").hover
+	(function(){
+		document.getElementById('v15').play();
+});
+
+$(".v16").hover
+	(function(){
+		document.getElementById('v16').play();
+});
+
 // BELLS
 $(".b1").hover
 	(function(){
@@ -83,6 +164,132 @@ $(".b12").hover
 });
 
 // STRING
+$(".s1").hover
+	(function(){
+		document.getElementById('s1').play();
+});
+
+$(".s2").hover
+	(function(){
+		document.getElementById('s2').play();
+});
+
+$(".s3").hover
+	(function(){
+		document.getElementById('s3').play();
+});
+
+$(".s4").hover
+	(function(){
+		document.getElementById('s4').play();
+});
+
+$(".s5").hover
+	(function(){
+		document.getElementById('s5').play();
+});
+
+$(".s6").hover
+	(function(){
+		document.getElementById('s6').play();
+});
+
+$(".s7").hover
+	(function(){
+		document.getElementById('s7').play();
+});
+
+$(".s8").hover
+	(function(){
+		document.getElementById('s8').play();
+});
+
+$(".s9").hover
+	(function(){
+		document.getElementById('s9').play();
+});
+
+$(".s10").hover
+	(function(){
+		document.getElementById('s10').play();
+});
+
+$(".s11").hover
+	(function(){
+		document.getElementById('s11').play();
+});
+
+$(".s12").hover
+	(function(){
+		document.getElementById('s12').play();
+});
+
+$(".s13").hover
+	(function(){
+		document.getElementById('s13').play();
+});
+
+$(".s14").hover
+	(function(){
+		document.getElementById('s14').play();
+});
+
+$(".s15").hover
+	(function(){
+		document.getElementById('s15').play();
+});
+
+$(".s16").hover
+	(function(){
+		document.getElementById('s16').play();
+});
+
+$(".s17").hover
+	(function(){
+		document.getElementById('s17').play();
+});
+
+$(".s18").hover
+	(function(){
+		document.getElementById('s18').play();
+});
+
+$(".s19").hover
+	(function(){
+		document.getElementById('s19').play();
+});
+
+$(".s20").hover
+	(function(){
+		document.getElementById('s20').play();
+});
+
+$(".s21").hover
+	(function(){
+		document.getElementById('s21').play();
+});
+
+$(".s22").hover
+	(function(){
+		document.getElementById('s22').play();
+});
+
+$(".string-portal").hover
+	(function(){
+		document.getElementById('s23').play();
+});
+
+$(".s24").hover
+	(function(){
+		document.getElementById('s24').play();
+});
+
+$(".s25").hover
+	(function(){
+		document.getElementById('s25').play();
+});
+
+// CHIMES
 $(".c1").hover
 	(function(){
 		document.getElementById('c1').play();
